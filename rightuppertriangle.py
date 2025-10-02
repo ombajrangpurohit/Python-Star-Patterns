c0994b1b6cfb665540045f1d87ebda73f2c8f98d
Om Bajrang Purohit <ombajrangpurohit064@gmail.com>
2025-10-02 18:01:25 +0530

    updated

--- rightuppertriangle.py
+++ rightuppertriangle.py
@@ -2,7 +2,7 @@ rows = 5
 for i in range(rows):
     for j in range(rows):
         if i <= j:
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
