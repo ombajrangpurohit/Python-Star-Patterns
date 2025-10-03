6f6eeb048d8b6e3a6342282b297f38afce26ff80
Om Bajrang Purohit <ombajrangpurohit064@gmail.com>
2025-10-03 11:03:23 +0530

    added

---
+++ Hole.py
@@ -0,0 +1,13 @@
+rows = 4
+for i in range(rows):
+    for j in range(rows):
+        if i >= j or j <= rows - i - 1:
+            print("#", end=" ")
+        else:
+            print(" ", end=" ") 
+    for j in range(rows):
+        if i <= j or j >= rows - i - 1 :
+            print("#", end = " ")
+        else:
+            print(" ", end = " ")
+    print()
---
+++ leftmixed.py
@@ -0,0 +1,12 @@
+rows = 5
+for i in range(rows):
+    for j in range(rows):
+        if i >= j or j <= rows - i - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ") 
+    print()
+
+
+
+
